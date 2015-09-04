247d53c21197bf797e9545ee14e240a5fb0bcc21
Virgis <virgis@kitoks.lt>
2015-09-04 16:49:08 +0300

    Lemma updated

--- Owl/LmfLemma.php
+++ Owl/LmfLemma.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 namespace Rastija\Owl;
-use Rastija\Owl\Uri;
+use Rastija\Owl\Uri\AbstractUri;
 /**
  * Description of LmfLemma
  *
@@ -14,11 +14,94 @@ use Rastija\Owl\Uri;
  */
 class LmfLemma extends AbstractLmfForm implements LmfClassInterface 
 {
+    /**
+     * Lemma uri class
+     * 
+     * @var AbstractUri
+     */
+    private $lemmaUri;
     
+    /**
+     * {@inheritdoc}
+     */
+    public function getUri() {
+        return $this->lemmaUri->getUri();
+    }
     
+    /**
+     * {@inheritdoc}
+     */    
+    public function setUri(AbstractUri $uri) {
+        $this->lemmaUri = $uri;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */    
     public function toLmfString() {
-        $str .= "<owl:NamedIndividual rdf:about=\"{$this->getUri()}\">\n";
+        $str = "<owl:NamedIndividual rdf:about=\"{$this->getUri()}\">\n";
         $str .= "\t<writtenForm>{$this->getWrittenForm() }</writtenForm>\n";
+        
+        if ($this->getAccentuation()) {
+            $str .= "\t<accentuation>{$this->getAccentuation() }</accentuation>\n";
+        }
+        
+        if ($this->getDeclension()) {
+            $str .= "\t<declension>{$this->getDeclension() }</declension>\n";
+        }
+
+        if ($this->getFormula()) {
+            $str .= "\t<formula>{$this->getFormula() }</formula>\n";
+        }
+        
+        if ($this->getGrammaticalCase()) {
+            $str .= "\t<grammaticalCase>{$this->getGrammaticalCase() }</grammaticalCase>\n";
+        }        
+        
+        if ($this->getGrammaticalGender()) {
+            $str .= "\t<grammaticalGender>{$this->getGrammaticalGender() }</grammaticalGender>\n";
+        }        
+     
+        if ($this->getGrammaticalGrade()) {
+            $str .= "\t<grammaticalGrade>{$this->getGrammaticalGrade() }</grammaticalGrade>\n";
+        }
+
+        if ($this->getGrammaticalNumber()) {
+            $str .= "\t<grammaticalNumber>{$this->getGrammaticalNumber() }</grammaticalNumber>\n";
+        }
+
+        if ($this->getGrammaticalTense()) {
+            $str .= "\t<grammaticalTense>{$this->getGrammaticalTense() }</grammaticalTense>\n";
+        }
+
+        if ($this->getGraphics()) {
+            $str .= "\t<graphics>{$this->getGraphics() }</graphics>\n";
+        }
+
+        if ($this->getImage()) {
+            $str .= "\t<image>{$this->getImage() }</image>\n";
+        }
+
+        if ($this->getOrhographyName()) {
+            $str .= "\t<orhographyName>{$this->getOrhographyName() }</orhographyName>\n";
+        }        
+
+        if ($this->getPerson()) {
+            $str .= "\t<person>{$this->getPerson() }</person>\n";
+        }
+
+        if ($this->getPhoneticForm()) {
+            $str .= "\t<phoneticForm>{$this->getPhoneticForm() }</phoneticForm>\n";
+        }
+
+        if ($this->getScript()) {
+            $str .= "\t<script>{$this->getScript() }</script>\n";
+        }
+
+        if ($this->getSound()) {
+            $str .= "\t<script>{$this->getSound() }</script>\n";
+        }
+        
         $str .= "\t<rdfs:label>{$this->getWrittenForm() }-Lemma</rdfs:label>\n";
         $str .= "\t<rdf:type rdf:resource=\"&lmf;Lemma\"/>\n";
         $str .= "</owl:NamedIndividual>\n";
